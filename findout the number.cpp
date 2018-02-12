edea24e1c90e68dc237cac89c9e8fb4345b00050
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2018-02-12 09:54:44 +0530

    Create findout the number.cpp

---
+++ findout the number.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+cout<<"enter the number";
+cin>>a;
+if(a>0)
+cout<<"positive";
+elsse if(a<0)
+cout<<'negative";
+else
+cout<<"zero";
+getch();
+}
