eff46c32aacb914d0f3e05636bb26f911a7d92d8
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2018-03-29 21:00:38 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+clrscr();
+printf("enter the value for N");
+scanf("%d,&n);
+for(i=1;i<=n;i++)
+{
+printf("HELLO\n");
+}
+getch();
+}
