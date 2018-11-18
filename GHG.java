514a6072699b6d819244f281834ffa18c255fe53
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2018-11-18 21:40:55 +0530

    Create GHG.java

---
+++ GHG.java
@@ -0,0 +1,19 @@
+class GFG
+{ 
+    static float sumOfAP(float a, float d,int n) 
+    { 
+        float sum = 0; 
+        for (int i = 0; i < n; i++) 
+        { 
+            sum = sum + a; 
+            a = a + d; 
+        } 
+        return sum; 
+    } 
+    public static void main(String args[]) 
+    { 
+        int n = 20; 
+        float a = 2.5f, d = 1.5f; 
+        System.out.println(sumOfAP(a, d, n)); 
+    } 
+} 
