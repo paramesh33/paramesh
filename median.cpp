409010e511955159cfba32861223af60b40de7a8
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2019-01-20 17:50:08 +0530

    Create median.cpp

---
+++ median.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+int main()
+{
+    std::vector<double> v;
+    int temp, len;
+    std::cin >> len;
+    while (v.size() < len)
+    {
+        std::cin >> temp;
+        v.push_back(temp);
+    }
+    sort(v.begin(), v.end());
+    if (v.size() % 2 == 0)
+        std::cout << std::endl ;
+                  << (v[v.size()/2 - 1] + v[v.size()/2]) / 2
+	          << std::endl;
+    else
+        std::cout << std::endl << v[v.size()/2]
+		  << std::endl;
+}
