d171f4110a5ce90e109fa7362932ddaae50b7d51
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2019-01-28 21:07:58 +0530

    Update fibopy.py

--- fibopy.py
+++ fibopy.py
@@ -1,7 +1,13 @@
-N=int(input());
-a=1;
-print(a);
-for x in range(n):
-    print(a);
-    a+=a;
-    
+nterms = int(input("How many terms? "));
+n1 = 0
+n2 = 1
+count = 0
+if nterms == 1:
+   print(n1)
+else:
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
