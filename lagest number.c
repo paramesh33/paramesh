a06bcc6a075918cbd37256824634275e325ba081
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2019-06-06 11:55:33 +0530

    Update lagest number.c

--- lagest number.c
+++ lagest number.c
@@ -1,20 +1,17 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int a,b,c;
-clrscr();
-printf("enter three numbers\n");
 scanf("%d%d%d",&a,&b,&c);
 if((a>b)&&(a>c))
 {
- printf("%d is the greatest number among three",a);
+ printf(a);
 }
 else if ((b>a)&&(b>c))
 {
-printf("%d is the greatest number among three",b);
+printf(b);
 }
 else
-printf("%d is the greatest number among three",c);
+printf(c);
 getch();
 }
