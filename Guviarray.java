394ff0e41c62b87cfcb4b2f5193c289ee9c68f66
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2019-06-06 12:20:31 +0530

    Update Guviarray.java

--- Guviarray.java
+++ Guviarray.java
@@ -5,21 +5,17 @@ class Guviarray
     {
       Scanner sc=new Scanner(System.in);
       int c=0;
-      System.out.println("enter the no of elements you want:");
       int n=sc.nextInt();
-      System.out.println("enter the element upto which you want to add: ");
       int p=sc.nextInt();
       int a[]=new int[n];
-      System.out.println("the values of array:");
       for(int i=0;i<n;i++)
          {
-               System.out.println("enter the values");
                a[i]=sc.nextInt();
           }
        for(int i=0;i<p;i++)
          {
              c=c+a[i];
           }
-       System.out.println("the sum of first "+p+" array elements is "+c);
+       System.out.println(c);
   }
 }
